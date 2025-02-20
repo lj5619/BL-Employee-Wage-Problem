4764df6a8a5b1bb0be2bbd8f4eb00734f2fe2a7c
lj5619 <leon.olaprath@gmail.com>
2025-02-21 00:41:51 +0530

    [Leon] Add: Python program to check attendance:

--- Employee.py
+++ Employee.py
@@ -1,2 +1,14 @@
 
 print('This is Employee wage problem')
+
+# Checking employee attendance
+import random
+
+def check_attendance():
+    attendance = random.randint(0,1)
+    if attendance == 0:
+        print('Employee is absent')
+    else:
+        print('Employee is present')
+
+check_attendance()
