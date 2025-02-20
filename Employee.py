494ed87f92260b2bf7d0f16c78f0f59ca12b81bd
lj5619 <leon.olaprath@gmail.com>
2025-02-21 00:56:22 +0530

    [Leon] Add: Python program to add Part Time

--- Employee.py
+++ Employee.py
@@ -1,14 +1,24 @@
-
-print('This is Employee wage problem')
-
-# Checking employee attendance
 import random
 
 def check_attendance():
-    attendance = random.randint(0,1)
-    if attendance == 0:
-        print('Employee is absent')
-    else:
-        print('Employee is present')
+    return random.randint(0,2)
 
-check_attendance()
+def daily_wage():
+
+    attendance = check_attendance()
+    salary = hours = 0
+    if attendance == 1:
+        hours = 8
+        print('Employee is full time and is present today')
+    elif attendance == 2:
+        hours = 4
+        print('Employee is part time and is present today')
+    else:
+        print('Employee is absent ')
+    salary = hours * 20
+
+    
+    print(f'The daily wage of employee is {salary}')
+
+daily_wage()
+
